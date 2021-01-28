d0114b9314eb46713149825d9002bb671dd04245
Jedich <alexyudakov483@gmail.com>
2021-01-28 17:03:59 +0200

    opencv sample

--- Assets/Scripts/Modules/CameraSensor.cs
+++ Assets/Scripts/Modules/CameraSensor.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.Collections;
+using System.Runtime.InteropServices;
+using System.Threading;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,6 +11,8 @@ public class CameraSensor : MonoBehaviour {
 	public Image testSprite;
 	private Rect spriteRect;
 	private Vector2 pivot;
+	public bool isComputed = false;
+	private int camWidth, camHeight;
 	
 	Texture2D ToTexture2D(RenderTexture rTex) {
 		Texture2D tex = new Texture2D(512, 512, TextureFormat.RGB24, false);
@@ -22,17 +26,37 @@ public class CameraSensor : MonoBehaviour {
 	private static extern void ProcessImage(ref Color32[] rawImage, int width, int height);
 
 	void Start() {
+		camWidth = camTexture.width;
+		camHeight = camTexture.height;
 		outputFrame = new Texture2D(camTexture.width, camTexture.height); 
 		spriteRect = new Rect(0, 0, camTexture.width, camTexture.height); 
 		pivot = new Vector2(0.5f, 0.5f);
+		rawFrame = ToTexture2D(camTexture).GetPixels32();
+		//StartCoroutine(CVCall());
+		Thread a = new Thread(VisionCall);
+		a.Start();
+	}
+	
+
+	void VisionCall() {
+		for (;;){
+			if (!isComputed) {
+				ProcessImage(ref rawFrame, camWidth, camHeight);
+				Thread.Sleep((int) (CarPreferences.map["cvCallDelay"] * 100));
+				isComputed = true;
+			}
+			//yield return new WaitForSeconds(CarPreferences.map["cvCallDelay"]);
+			//System.IO.File.WriteAllBytes (Application.dataPath + "/test.png", outputFrame.EncodeToPNG());
+		}
 	}
 
 	void Update() {
-		rawFrame = ToTexture2D(camTexture).GetPixels32();
-		ProcessImage(ref rawFrame, camTexture.width, camTexture.height);
-		outputFrame.SetPixels32(rawFrame);
-		outputFrame.Apply();
-		testSprite.sprite = Sprite.Create(outputFrame, spriteRect, pivot);
-		//System.IO.File.WriteAllBytes (Application.dataPath + "/test.png", outputFrame.EncodeToPNG());
+		if (isComputed) {
+			outputFrame.SetPixels32(rawFrame);
+			outputFrame.Apply();
+			testSprite.sprite = Sprite.Create(outputFrame, spriteRect, pivot);
+			rawFrame = ToTexture2D(camTexture).GetPixels32();
+			isComputed = false;
+		}
 	}
 }
